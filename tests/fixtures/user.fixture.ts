a6c2da6a0f6fc6cb7969c9ae3c0f2ed4fc47f44d
Adelodun Damilare <adelodundamilare@gmail.com>
2024-03-21 06:09:36 +0100

    fix deployment

--- tests/fixtures/user.fixture.ts
+++ tests/fixtures/user.fixture.ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcryptjs';
 import { faker } from '@faker-js/faker';
 import prisma from '../../src/client';
-import { Prisma, Role } from '@/prisma/generated/client';
+import { Prisma, EnumRole } from '@/prisma/generated/client';
 
 const password = 'password1';
 const salt = bcrypt.genSaltSync(8);
@@ -10,7 +10,7 @@ export const userOne = {
   name: faker.name.fullName(),
   email: faker.internet.email().toLowerCase(),
   password,
-  role: Role.USER,
+  role: EnumRole.USER,
   isEmailVerified: false
 };
 
@@ -18,7 +18,7 @@ export const userTwo = {
   name: faker.name.fullName(),
   email: faker.internet.email().toLowerCase(),
   password,
-  role: Role.USER,
+  role: EnumRole.USER,
   isEmailVerified: false
 };
 
@@ -26,7 +26,7 @@ export const admin = {
   name: faker.name.fullName(),
   email: faker.internet.email().toLowerCase(),
   password,
-  role: Role.ADMIN,
+  role: EnumRole.ADMIN,
   isEmailVerified: false
 };
 
