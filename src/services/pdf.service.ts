19a83636cd648c89d44c059207a951eca685f8fd
Adelodun Damilare <adelodundamilare@gmail.com>
2024-05-08 00:30:38 +0100

    formatting book

--- src/services/pdf.service.ts
+++ src/services/pdf.service.ts
@@ -6,35 +6,11 @@ import fs from 'fs/promises';
 import config from '../config/config';
 
 async function generatePDFfromHTML(htmlContents: string[], outputPath: string): Promise<string> {
-  // let timeInMil: any = new Date();
-  // timeInMil = timeInMil.getTime();
-
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const pdfDoc = await PDFDocument.create();
 
-  // for (let i = 0; i < htmlContents.length; i++) {
-  //   const url = htmlContents[i];
-  //   const cssPath = path.resolve('html/assets/style.css');
-  //   await page.addStyleTag({ path: cssPath });
-  //   await page.goto(url, { waitUntil: ['load', 'networkidle0', 'domcontentloaded'] });
-  //   const pdfBytes = await page.pdf({
-  //     format: 'A4',
-  //     printBackground: true,
-  //     margin: { top: '0px', left: '0px', right: '0px', bottom: '0px' }
-  //   });
-  //   const extPdfDoc = await PDFDocument.load(pdfBytes);
-  //   const copiedPages = await pdfDoc.copyPages(extPdfDoc, [0]);
-  //   copiedPages.forEach((page) => pdfDoc.addPage(page));
-  // }
-
-  // const mergedPdfBytes = await pdfDoc.save();
-  // await fs.writeFile('merged.pdf', mergedPdfBytes);
-  // await browser.close();
-
   for (const item of htmlContents) {
-    // console.log({ item });
-    // await page.goto(url, { waitUntil: ['load', 'networkidle0', 'domcontentloaded'] });
     await page.setContent(item, { waitUntil: ['load', 'networkidle0', 'domcontentloaded'] });
     const cssPath = path.resolve('html/assets/style.css');
     await page.addStyleTag({ path: cssPath });
@@ -52,13 +28,6 @@ async function generatePDFfromHTML(htmlContents: string[], outputPath: string): 
   await fs.writeFile(outputPath, mergedPdfBytes);
   await browser.close();
   console.log('Merged PDF created successfully');
-
-  // await page.setContent(htmlContent, { waitUntil: ['load', 'networkidle0', 'domcontentloaded'] });
-
-  // const cssPath = path.resolve('html/assets/style.css');
-  // await page.addStyleTag({ path: cssPath });
-  // await page.pdf({ path: outputPath, format: 'A4', printBackground: true }); // Set landscape option to true
-  // await browser.close();
 
   const baseUrl = config.baseUrl;
   return `${baseUrl}/${path.basename(outputPath)}`;
